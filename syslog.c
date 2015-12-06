4e076a61dff2ce590c4cda6032b8bc600751da96
vitalinobr <vitalinobr@gmail.com>
2015-12-06 20:34:16 -0200

    Update syslog.c

--- syslog.c
+++ syslog.c
@@ -1,10 +1,22 @@
+/*
+	Exemplo de uso da função setlogmask(int mask)
+	Visualize a escrita do log com:
+	# tail -f /var/log/auth.log
+
+	Neste exemplo, a função setlogmask é utilizada para
+	permitir somente a chamada ao syslog para que a mensagem
+	com o level LOG_DEBUG seja executada.
+*/
+
 #include <stdio.h>
 #include <syslog.h>
 
 int main(void) {
+
+	/* Experimente comentar e descomentar a linha á baixo */
 	setlogmask(LOG_MASK(LOG_DEBUG));
 
-	syslog(LOG_AUTH | LOG_EMERG, "[auth] EMERGÊNCIA !!!");
+	//syslog(LOG_AUTH | LOG_EMERG, "[auth] EMERGÊNCIA !!!");
 	syslog(LOG_AUTH | LOG_ALERT, "[auth] ALERTA !!!");
 	syslog(LOG_AUTH | LOG_CRIT, "[auth] CRÍTICO !!!");
 	syslog(LOG_AUTH | LOG_ERR, "[auth] ERRO !!!");
